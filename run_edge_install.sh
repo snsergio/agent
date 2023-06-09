00f6d82d50743827fa6c625058baf0139d4a9b46
Sergio Siqueira <snsergio@hotmail.com>
2023-06-09 09:46:16 -0300

    initial release

---
+++ run_edge_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+clear
+set -eo pipefail
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit
+fi
+curl https://raw.githubusercontent.com/snsergio/agent/main/install-edge.sh
+chmod +x install-edge.sh
+source /opt/eyeflow/install/install-monitor.sh
+echo "End of installation"
