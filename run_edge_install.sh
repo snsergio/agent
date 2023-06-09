e19004047d91ad741ae0106c2e1b5429d43636c4
Sergio Siqueira <snsergio@hotmail.com>
2023-06-09 10:37:10 -0300

    Delete run_edge_install.sh

--- run_edge_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# How to run: curl https://raw.githubusercontent.com/snsergio/agent/main/run_edge_install.sh | bash
-wget https://raw.githubusercontent.com/snsergio/agent/main/install-edge.sh
-sudo mv ./install-edge.sh /opt/eyeflow/install/install-edge.sh
-chmod a+x /opt/eyeflow/install/install-edge.sh
-sudo bash /opt/eyeflow/install/install-edge.sh
-echo "End of installation"
-rm -f install-edge.sh
