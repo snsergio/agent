60980b0587e3964ba1340a4b383d990ac8a62d66
Sergio Siqueira <snsergio@hotmail.com>
2023-06-09 10:13:27 -0300

    Update run_edge_install.sh

--- run_edge_install.sh
+++ run_edge_install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # How to run: curl https://raw.githubusercontent.com/snsergio/agent/main/run_edge_install.sh | bash
 wget https://raw.githubusercontent.com/snsergio/agent/main/install-edge.sh
-echo $(pwd)
-ls -l
-sudo mv install-monitor.sh /opt/eyeflow/install/install-monitor.sh
-chmod a+x /opt/eyeflow/install/install-monitor.sh
-sudo source /opt/eyeflow/install/install-monitor.sh
+sudo mv ./install-edge.sh /opt/eyeflow/install/install-edge.sh
+chmod a+x /opt/eyeflow/install/install-edge.sh
+sudo source /opt/eyeflow/install/install-edge.sh
 echo "End of installation"
