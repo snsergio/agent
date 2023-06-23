370d509d4e9b8649130afabc3f44ec3859a1f5c3
Sergio Siqueira <snsergio@hotmail.com>
2023-06-23 15:30:05 -0300

    Create metricjetson.py

---
+++ lib/metricjetson.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#######################################################################################################################
+versao = "metricjetson-v5.00-beta-test"
+#######################################################################################################################
+import logging
+import time
+from lib import common as c
+#######################################################################################################################
+c.versionDict["metricjetson"] = versao
+#######################################################################################################################
+class jetson_exec:
+    def collect_jetson(getJetson):
+        import os
+        jetsonout = {}
+        if getJetson:
+            jetsonPath = "/usr/local/cuda/samples/1_Utilities/deviceQuery/deviceQuery"
+            # isJetson = os.path.exists(jetsonPath)
+            if os.path.exists(jetsonPath):
+                if not c.logFirstRun: logging.info(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))}-metricjetson version: {versao}")
+                try: jetsonout = c.exec_cmd([jetsonPath], c.debugMode)["output"].splitlines()
+                except: 
+                    if not c.logFirstRun: logging.error(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))}-jetson_exec.collect_jetson: Device Query execution error")
+        if jetsonout != 0: jetsonMetrics = jetson_exec.jetson_metrics(jetsonout, c.logFirstRun)
+        else: jetsonMetrics = 0
+        return jetsonMetrics
+        #----------------------------------------------------------------------------------------------------------------------
+    def collect_jtop(resposta):
+        from jtop import jtop, JtopException
+        try:
+            with jtop() as jetson:
+                if jetson.ok():
+                    jtopout = jetson.stats
+        except JtopException as e: 
+            if not c.logFirstRun: logging.error(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))}-jetson_exec.collect_jtop: jtop error")
+            logging.error(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))}-jetson_exec.collect_jtop: jtop error: {e}")
+        else:
+            resposta["cpuTotalCount"] = 0
+            for k, v in jtopout.items():
+                if (len(k) <= 5) and ("CPU" in k): resposta["cpuTotalCount"] += 1
+                if (len(k) == 3) and ("GPU" in k): resposta["gpuUse"] = int(v)
+                if ("Fan" in k): resposta["fanPercent"] = int(v)
+                if ("TOT" in k): resposta["powerTotal"] = int(v)
+                if ("thermal" in k): resposta["powerThermal"] = int(v)
+                if ("RAM" in k): resposta["ramUsed"] = float(v)
+                if ("Temp CPU" in k): resposta["tempCPU"] = float(v)
+                if ("Temp GPU" in k): resposta["tempGPU"] = float(v)
+                if ("Tboard" in k): resposta["tempBoard"] = float(v)
+                if ("iwlwifi" in k): resposta["tempWifi"] = float(v)
+                if ("nvp" in k): resposta["nvpModel"] = str(v)
+                if (k == "time"): resposta["time"] = v.timestamp()
+                if (k == "uptime"): resposta["uptime"] = v.total_seconds()
+        return resposta
+        #----------------------------------------------------------------------------------------------------------------------
+    def jetson_metrics(jetsonData, logFirstRun):
+        resposta = {
+            "card": "",
+            "driver": "",
+            "cuda": "",
+            "gpuName": "",
+            "memTotalMB": 0}
+        for item in range(len(jetsonData)):
+            if (4 <= item <= 7) and ("Device" in jetsonData[item]): resposta["card"] = jetsonData[item].split(":")[0].split()[-1].strip()
+            if (5 <= item <= 8) and ("Driver" in jetsonData[item]) and ("CUDA" in jetsonData[item]): resposta["driver"] = jetsonData[item].split()[-1].strip()
+            if (5 <= item <= 8) and ("Driver" in jetsonData[item]) and ("CUDA" in jetsonData[item]): resposta["cuda"] = jetsonData[item].split("/")[1].split()[-1]
+            if (5 <= item <= 7) and ("Device" in jetsonData[item]): resposta["gpuName"] = str(jetsonData[item].split(":")[-1].strip().replace("'","").replace('"', ''))
+            if (5 <= item <= 10) and ("amount" in jetsonData[item]) and ("global" in jetsonData[item]): resposta["memTotalMB"] = int(jetsonData[item].split(":")[1].split()[0])
+        try: a = jetson_exec.collect_jtop(resposta)
+        except: 
+            if not c.logFirstRun: logging.error(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))}-jetson_exec.collect_jetson: Device Query execution error")
+        return resposta
+        #----------------------------------------------------------------------------------------------------------------------
