08d69f791e056e50024404c2ea827d0060688633
snsergio <snsergio@hotmail.com>
2023-08-16 12:57:06 +0000

    [GHA] Release to public repo

--- install/upgrade-monitor.sh
+++ install/upgrade-monitor.sh
@@ -9,7 +9,7 @@ if [ "$EUID" -ne 0 ]
     then echo "Please run as root"
     exit
 fi
-LOGFILE="/opt/eyeflow/install/monitor-install.log"
+LOGFILE="/opt/eyeflow/monitor/install/monitor-install.log"
 if [ -d /opt/eyeflow/monitor ]; then
     mv /opt/eyeflow/monitor /opt/eyeflow/monitor-old
 fi
@@ -26,10 +26,10 @@ else
     echo "Previous version not found... Exiting"
     exit
 fi
-touch /opt/eyeflow/install/monitor-install.log
+touch /opt/eyeflow/monitor/install/monitor-install.log
 echo "----- Starting Metric Collector Upgrade -----" | sudo tee -a $LOGFILE
 date | sudo tee -a $LOGFILE
-echo "Previous metric collector version '{$VERSAO}'" | sudo tee -a $LOGFILE
+echo "Previous metric collector version '$VERSAO'" | sudo tee -a $LOGFILE
 echo "----- Installing initial packages -----" | sudo tee -a $LOGFILE
 apt install -y curl lm-sensors sysstat ntpstat netcat iproute2 python3-requests python3-pip unzip
 echo "----- Installing GIT -----" | sudo tee -a $LOGFILE
@@ -44,8 +44,8 @@ if [ $(uname -i) == "aarch64" ]; then
     python3 -m pip install -U jetson-stats
 fi
 echo "----- Cloning Edge repo and setting rights -----" | sudo tee -a $LOGFILE
-cd /opt/eyeflow/install
-rm -rf /opt/eyeflow/install/agent
+cd /opt/eyeflow/monitor/install
+rm -rf /opt/eyeflow/monitor/install/agent
 git clone https://github.com/snsergio/agent.git
 if id "eyeflow" >/dev/null 2>&1; then
     chown -R eyeflow:users /opt/eyeflow/monitor
@@ -58,15 +58,17 @@ chmod g+rwxs /opt/eyeflow/monitor
 chmod 775 /opt/eyeflow/monitor
 echo "----- Deleting unnecessary files -----" | sudo tee -a $LOGFILE
 if [ $(uname -i) != "aarch64" ]; then
-  rm -rf /opt/eyeflow/install/agent/jetson
+  rm -rf /opt/eyeflow/monitor/install/agent/jetson
 fi
-rm -rf /opt/eyeflow/install/agent/install
-rm -rf /opt/eyeflow/install/agent/README*
-rm -rf /opt/eyeflow/install/agent/grafana-stack
+rm -rf /opt/eyeflow/monitor/install/agent/install
+rm -rf /opt/eyeflow/monitor/install/agent/README*
+rm -rf /opt/eyeflow/monitor/install/agent/grafana-stack
 echo "----- Adding files -----" | sudo tee -a $LOGFILE
-rsync -zvrh /opt/eyeflow/install/agent/* /opt/eyeflow/monitor
+rsync -zvrh /opt/eyeflow/monitor/install/agent/* /opt/eyeflow/monitor
 mv /opt/eyeflow/monitor/metric-collector/* /opt/eyeflow/monitor
 rm -rf /opt/eyeflow/monitor/metric-collector
+rm -rf /opt/eyeflow/monitor/prometheus-proxy
+rm -rf /opt/eyeflow/monitor/install/agent
 rm -rf /opt/eyeflow/monitor/README*
 chmod +x /opt/eyeflow/monitor/lib/pushprox-client
 echo "----- Preparing Collector -----" | sudo tee -a $LOGFILE
@@ -74,12 +76,13 @@ cp /opt/eyeflow/monitor/metric-collector.service /etc/systemd/system
 cp /opt/eyeflow/monitor/proxy-exporter.service /etc/systemd/system
 systemctl enable metric-collector.service
 systemctl enable proxy-exporter.service
+systemctl daemon-reload
 echo "----- Host FQDN: '$(hostname --fqdn)' -----" | sudo tee -a $LOGFILE
 clear
 echo "+---------------------------------------------------------------------------------------+"
 echo "! Edit and configure collector-config.yaml file                                         !"
 echo "! - The previous configuration file is at /opt/eyeflow/monitor-old                      !"
-echo "!   existing version is '{$VERSION}'                                                          !"
+echo "!   existing version is '$VERSAO'                                                          !"
 echo "! - If the collector will run as EXPORTER, configure Prometheus to scrape this host at: !"
 echo "!   FQDN: '$(hostname --fqdn)'                                                          !"
 echo "! - Then start metric collector and proxy exporter:                                     !"
