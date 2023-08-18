b45153857a5628db7b142e52aa70cd68a84029a9
snsergio <snsergio@hotmail.com>
2023-08-18 21:53:42 +0000

    [GHA] Release to public repo

--- install/docker-correct.sh
+++ install/docker-correct.sh
@@ -9,24 +9,20 @@ if [ "$EUID" -ne 0 ]; then
     echo "Please run as root"
     exit
 fi
-cd /opt/eyeflow/monitor/
 if [ ! -f /opt/eyeflow/monitor/metric-collector-v5.py ]; then
     echo "There is no metric-collector-v5.py file in this folder, exiting..."
     exit
 else
-    rm metric-collector-v5.py
-    wget https://raw.githubusercontent.com/snsergio/agent/main/metric-collector/metric-collector-v5.py
+    rm -f /opt/eyeflow/monitor/metric-collector-v5.py
+    wget -P /opt/eyeflow/monitor/ https://raw.githubusercontent.com/snsergio/agent/main/metric-collector/metric-collector-v5.py
 fi
-cd lib
 if [ ! -f /opt/eyeflow/monitor/lib/metricexporter.py ]; then
     echo "There is no metricexporter.py file in this folder, exiting..."
     exit
 else
-    rm metricexporter.py
-    wget https://raw.githubusercontent.com/snsergio/agent/main/metric-collector/lib/metricexporter.py
+    rm -f /opt/eyeflow/monitor/lib/metricexporter.py
+    wget -P /opt/eyeflow/monitor/lib/ https://raw.githubusercontent.com/snsergio/agent/main/metric-collector/lib/metricexporter.py
 fi
-cd $HOME
-cd ~
 systemctl restart metric-collector.service
 systemctl status metric-collector.service --no-pager
 echo "DONE!"
